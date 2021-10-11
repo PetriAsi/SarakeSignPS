7e05c1e982654240ca951951e133b344a818b791
Petri Asikainen <petri.asikainen@sataedu.fi>
2021-10-11 12:57:41 +0300

    Util to convert hashtables to name values pairs

---
+++ SarakeSignPS/Public/ConvertTo-NameValues.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+Cnvert hashtable to array of hashtables nad name value pairs
+#>
+
+function ConvertTo-NameValues {
+    [CmdletBinding()]
+    param (
+        # input hashtable
+        [Parameter(Mandatory=$true)]
+        [hashtable]
+        $hashtable
+    )
+
+    begin {
+        $output = @()
+    }
+
+    process {
+        $hashtable.GetEnumerator() | ForEach-Object{
+            $toAdd = @{}
+            $toAdd['values'] = [array]($_.value)
+            $toAdd['name'] = $_.key
+            $output += $toAdd.clone()
+        }
+
+    }
+
+    end {
+        $output
+    }
+}
