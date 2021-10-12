b8a8849c252ebbb51ee64e1cb78903ed0937b0e5
Petri Asikainen <petri.asikainen@sataedu.fi>
2021-10-12 13:51:27 +0300

    abort-signrequest

---
+++ SarakeSignPS/Public/Abort-SignRequest.ps1
@@ -0,0 +1,37 @@
+<#
+.SYNOPSIS
+This operation resets the signing request back to Draft status
+#>
+function Abort-SignRequest {
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        # Request id
+        [Parameter(Mandatory=$true,ValueFromPipelineByPropertyName)]
+        [string]
+        $id,
+        # Reason for abort
+        [Parameter(Mandatory=$true)]
+        [string]
+        $reason
+    )
+
+    begin {
+        $body = @{ "reason" = $reason}
+    }
+
+    process {
+        if ($PSCmdlet.ShouldProcess("ShouldProcess?")) {
+            foreach ($reqid in $id) {
+                $api = "/request/$reqid/abort"
+                $result = Invoke-SignApi -method PUT -api $api -body $body
+                if ($result) {
+                    $result
+                }
+            }
+        }
+    }
+
+    end {
+
+    }
+}
--- SarakeSignPS/SarakeSignPS.psd1
+++ SarakeSignPS/SarakeSignPS.psd1
@@ -70,10 +70,12 @@ PowerShellVersion = '7.1'
 
 # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
 FunctionsToExport = @(
-   'Connect-SarakeSignPS',
-   'ConvertTo-NameValues',
+
+   'Abort-SignRequest'
    'Add-SignDocument',
    'Add-SignRequestRecipient',
+   'Connect-SarakeSignPS',
+   'ConvertTo-NameValues',
    'Get-SignDocument',
    'Get-SignProcess',
    'Get-SignReqTemplate',
@@ -83,7 +85,8 @@ FunctionsToExport = @(
    'New-SignRequest',
    'Send-SignRequest',
    'Set-SignRequest',
-   'Remove-SignRequest'
+   'Remove-SignRequest',
+   'Reset-SignRequest'
 
 )
 
